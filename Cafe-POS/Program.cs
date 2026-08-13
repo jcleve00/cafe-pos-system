63dffb9fe0bad85a2a0e2ffdea04441bebd51259
jcleve00 <jcleve00@icloud.com>
2026-08-13 15:10:26 +0000

    HttpsRedirect

--- Cafe-POS/Program.cs
+++ Cafe-POS/Program.cs
@@ -25,6 +25,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-//app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
 
 app.Run();
